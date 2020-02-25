b94e7843f70db0812d3a9c5e0f26c622a9e6d38b
raquelacevedo <raquel.e.acevedo@gmail.com>
2020-02-25 14:42:46 -0500

    Food Nutrition Data and Tbl Schema

---
+++ db/nutrition_summary_tbl_schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE FdNutritionSummary (
+  FoodId int NOT NULL,
+  FdGrp_Desc varchar(50) not null,
+  FdGrp_Cd varchar(10) null,
+  FdName varchar(200) not null,
+  Amount float not null,
+  Msre_Desc varchar(100) ,
+  Gm_Wgt float null,
+  msre varchar(100) null,
+  Calories float not null,
+  Protein float not null,
+  Fat float not null,
+  Carbohydrates float not null,
+  PRIMARY KEY (FoodId)
+) 
