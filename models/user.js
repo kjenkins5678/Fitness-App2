71592ffb933494af1d34fdedc938a3e2c916f65e
Madison Schrews <maddyschrews@gmail.com>
2020-03-04 18:41:50 -0500

    user.js linting

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@
 
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
+    // eslint-disable-next-line camelcase
     user_name: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -12,76 +13,84 @@ module.exports = function(sequelize, DataTypes) {
     },
     age: {
       type: DataTypes.INTEGER,
-      allowNull: false, 
+      allowNull: false,
       validate: {
-        isNumeric: true, 
-        min:8, 
-        max:100
+        isNumeric: true,
+        min: 8,
+        max: 100
       }
     },
+    // eslint-disable-next-line camelcase
     height_inches: {
       type: DataTypes.INTEGER,
-      allowNull:false, 
-      validate:{
-        isNumeric: true,
-        min:48, 
-        max:100
-      } 
-    }, 
-    height_cm: {
-      type: DataTypes.DECIMAL, 
-      allowNull: true,
-      validate:{
-        isNumeric: true
-      }
-    }, 
-    weight_lb:{
-      type:DataTypes.INTEGER, 
       allowNull: false,
-      validate:{
-        isNumeric:true, 
-        min:80, 
-        max:600
-      }
-    }, 
-    weight_kg: {
-      type:DataTypes.DECIMAL, 
-      allowNull: true,
-      validate:{
-        isNumeric:true
-      }
-    }, 
-    gender: {
-      type:DataTypes.STRING,
-      validate:{
-        isAlpha: true,
-        isIn: [['Male', 'Female']]
+      validate: {
+        isNumeric: true,
+        min: 48,
+        max: 100
       }
     },
-    activity_level:{
-      type:DataTypes.STRING,
-      validate:{
-        isAlpha:true,
-        isIn:[['Sedentary','Moderate','Vigorous']]
+    // eslint-disable-next-line camelcase
+    height_cm: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+      validate: {
+        isNumeric: true
+      }
+    },
+    // eslint-disable-next-line camelcase
+    weight_lb: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+        min: 80,
+        max: 600
+      }
+    },
+    // eslint-disable-next-line camelcase
+    weight_kg: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+      validate: {
+        isNumeric: true
+      }
+    },
+    gender: {
+      type: DataTypes.STRING,
+      validate: {
+        isAlpha: true,
+        isIn: [["Male", "Female"]]
+      }
+    },
+    // eslint-disable-next-line camelcase
+    activity_level: {
+      type: DataTypes.STRING,
+      validate: {
+        isAlpha: true,
+        isIn: [["Sedentary", "Moderate", "Vigorous"]]
       }
     },
     goal: {
-      type:DataTypes.STRING,
+      type: DataTypes.STRING,
       validate: {
         isAlpha: true,
-        isIn:[['Lose Weight','Gain','Recomp']]
+        isIn: [["Lose Weight", "Gain", "Recomp"]]
       }
-    }, 
+    },
+    // eslint-disable-next-line camelcase
     fat_per_day: {
-      type:DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL,
       allowNull: true
-    }, 
+    },
+    // eslint-disable-next-line camelcase
     carbs_per_day: {
-      type:DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL,
       allowNull: true
-    }, 
+    },
+    // eslint-disable-next-line camelcase
     protein_per_day: {
-      type:DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL,
       allowNull: true
     }
   });
